e618ede4c5065b0b01da3423113a4a33689ab048
venkat-subbu <vs2707@gmail.com>
2023-04-12 08:09:13 -0400

    Final Changes

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,39 @@
+'''
+Python code to translate text between english and french
+'''
+
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def english_to_french(english_text):
+    '''
+    Convert english text to french
+    '''
+    french_text = '' if (english_text == '') else language_translator.translate(
+    text=english_text,
+    model_id='en-fr').get_result()['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    '''
+    Convert french text to english
+    '''
+    english_text = '' if (french_text == '') else language_translator.translate(
+    text=french_text,
+    model_id='fr-en').get_result()['translations'][0]['translation']
+    return english_text
--- final_project/server.py
+++ final_project/server.py
@@ -1,4 +1,5 @@
 from machinetranslation import translator
+from translator import english_to_french,french_to_english
 from flask import Flask, render_template, request
 import json
 
@@ -7,18 +8,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    frenchText = english_to_french(textToTranslate)
+    return frenchText
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    englishText = french_to_english(textToTranslate)
+    return englishText
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return "Translation Service between English and French"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
